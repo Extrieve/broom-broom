31be0e01eb02a84f18e8990e614de302c61b800c
Nick <nicolas.cinera@gmail.com>
2023-06-17 09:56:27 -0400

    New Assets

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from "next/image";
 import CustomButton from "./CustomButton";
 
 function Hero() {
@@ -19,6 +20,12 @@ function Hero() {
           containerStyles={'bg-primary-blue text-white rounded-full mt-10'}
           handleClick={handleScroll}
         />
+      </div>
+
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src='./hero.png' alt="hero"/>
+        </div>
       </div>
     </div>
   );
