5ef52369193328b03b0b738f690a425d10000224
Nick <nicolas.cinera@gmail.com>
2023-06-18 18:01:41 -0400

    Closing Div Fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,10 +25,10 @@ function Hero() {
       <div className="hero__image-container">
         <div className="hero__image">
           <Image src='./hero.png' alt="hero" />
+        </div>
           
-          <div className="hero__image-overlay">
+        <div className="hero__image-overlay">
             
-          </div>
         </div>
       </div>
     </div>
