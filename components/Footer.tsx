a56ed5aa6dbf54a36380229a3ebd29911936e3b6
Nick <nicolas.cinera@gmail.com>
2023-06-18 10:22:50 -0400

    Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,22 @@
+import Image from "next/image";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="flex flex-col text-blue-100 mt-5 border-t border-gray-100">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image src="/logo.svg" alt="logo" width="118" height="18" />
+          <p className="text-bae text-gray-700">
+            Carhub 2023 <br />
+            All Rights Reserved &copy;
+          </p>
+        </div>
+        <div className="footer__links">
+          
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
