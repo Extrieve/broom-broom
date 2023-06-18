169dbb41f530b9da4d8df24c7418283381761925
Nick <nicolas.cinera@gmail.com>
2023-06-18 18:07:01 -0400

    Starting Offering Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
-import { Hero } from '../components/index'
+import { Hero } from '../components/index';
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold"> Car Catalogue</h1>
+          <p>Explore vehicles of your liking</p>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
