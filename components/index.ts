c795e58bdd3958476cbd4510c43fd371fbe4002f
Nick <nicolas.cinera@gmail.com>
2023-06-19 11:02:10 -0400

    New Comps

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,12 @@
 import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import Footer from "./Footer";
 import Hero from "./Hero";
+import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
 
-export { CustomButton, Hero };
+export {
+    CustomButton, CustomFilter, Footer, Hero,
+    Navbar, SearchBar
+};
 
