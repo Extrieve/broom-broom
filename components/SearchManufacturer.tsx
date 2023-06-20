930493f805a57c08ff04bd0445c920dd0b9c7200
Nick <nicolas.cinera@gmail.com>
2023-06-19 20:11:48 -0400

    ComboBox

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,9 +1,12 @@
 'use client';
 import { SearchManufacturerProps } from "@/types";
-import { Combobox } from "@headlessui/react";
+import { Combobox, Transition } from "@headlessui/react";
 import Image from "next/image";
+import { Fragment, useState } from "react";
 
 function SearchManufacturer({manufacturer, setManufacturer}: SearchManufacturerProps) {
+  const [query, setQuery] = useState('');
+  
   return (
     <div className="search-manufacturer">
       <Combobox>
@@ -17,6 +20,22 @@ function SearchManufacturer({manufacturer, setManufacturer}: SearchManufacturerP
               className="ml-14"
             />
           </Combobox.Button>
+
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="Search manufacturer"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery('')}
+          >
+          </Transition>
         </div>
       </Combobox>
     </div>
