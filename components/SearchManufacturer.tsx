ae720ff8f0f33b02a2a1e425eef1b658b9c7a549
Nick <nicolas.cinera@gmail.com>
2023-06-19 20:04:41 -0400

    Applying the ComboBox

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,8 +1,25 @@
+'use client';
 import { SearchManufacturerProps } from "@/types";
+import { Combobox } from "@headlessui/react";
+import Image from "next/image";
 
 function SearchManufacturer({manufacturer, setManufacturer}: SearchManufacturerProps) {
   return (
-    <div>SearchManufacturer</div>
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              alt="car logo"
+              className="ml-14"
+            />
+          </Combobox.Button>
+        </div>
+      </Combobox>
+    </div>
   );
 }
 
